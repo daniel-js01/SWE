944cff60db59353ebc100b3657217b96f9c570de
daniel-js01 <daniel.yc.ju@gmail.com>
2026-06-01 16:03:03 +0900

    Update Practice.java - add arithmetic methods

--- Jenkins/src/practice/Practice.java
+++ Jenkins/src/practice/Practice.java
@@ -1,10 +1,10 @@
 package practice;
 
 public class Practice {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
 }
